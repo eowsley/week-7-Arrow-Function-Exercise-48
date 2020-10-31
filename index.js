cbccacd4d27d12a23ac2a30d040d3f0ec8f4eda1
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-30 17:15:59 -0700

    final submit

---
+++ index.js
@@ -0,0 +1,9 @@
+// write an arrow function expression called greet.
+// It should accept a single string argument, a person's name.
+// return a greeting string
+
+const greet = (str) => {
+    return `Hey ${str}!`
+}
+
+console.log(greet("Sarah"))
